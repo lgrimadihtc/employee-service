8cd51aeb8e6f22005a72e92801c50729b19c64f5
lgrimaldi <lgrimaldi@hightech-corp.com>
2022-02-14 07:06:58 -0700

    solved issue with conexion

--- src/main/java/com/htc/microservices/employee/exception/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/htc/microservices/employee/exception/CustomResponseEntityExceptionHandler.java
@@ -1,16 +1,21 @@
 package com.htc.microservices.employee.exception;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 @ControllerAdvice
 @RestController
@@ -25,7 +30,7 @@ public class CustomResponseEntityExceptionHandler   extends ResponseEntityExcept
 
 				request.getDescription(false));
 
-		return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 
 	}
 	
@@ -41,19 +46,30 @@ public class CustomResponseEntityExceptionHandler   extends ResponseEntityExcept
 
 	}
 	
+	
+
 	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+	  MethodArgumentNotValidException ex, 
+	  HttpHeaders headers, 
+	  HttpStatus status, 
+	  WebRequest request) {
+	    List<String> errors = new ArrayList<String>();
+	    for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+	        errors.add(error.getField() + ": " + error.getDefaultMessage());
+	    }
+	    for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+	        errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+	    }
+	    
+	
+		ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), ex.getMessage(),
 
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+				request.getDescription(false));
 
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-
-		ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), "Validation Failed",
-
-				ex.getBindingResult().toString());
-
-		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
-
-	}	
+	    return handleExceptionInternal(
+	      ex, exceptionResponse, headers,HttpStatus.BAD_REQUEST, request);
+	}
 
 
 }
